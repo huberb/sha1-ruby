06f1a47a204db030c2e4ff5abfb3c4e133413d69
Ben <benedikthuber44@googlemail.com>
2018-04-25 19:07:08 +0200

    refactor

--- sha1.rb
+++ sha1.rb
@@ -1,7 +1,9 @@
 class Sha1
     def initialize(message)
+        # the message as a string of bits
         @bitstring = message.unpack('B*')[0]
         @orig_message_len = @bitstring.length
+        # initial values for compression function
         @hash_words = { 
             a: 0x67452301,
             b: 0xEFCDAB89, 
@@ -21,20 +23,26 @@ class Sha1
         end
         # append ml, the original message length,
         # as a 64-bit big-endian integer.
-        # Thus, the total length is a multiple of 512 bits.
+        # thus, the total length is a multiple of 512 bits.
         @bitstring += (("0" * (64 - @orig_message_len.to_s(2).length)) + @orig_message_len.to_s(2))
     end
 
+    # split up a string into chunks of a given size
+    # works with and without block
     def split_up(string, size)
         strings = (string.length / size).times.collect { |i| string[i * size, size] }
         strings.each{ |s| yield s } if block_given?
         strings
     end
 
+    # rotate a number bitwise, keep at 32 bit length
+    # kind of hard to understand how this works:
+    # https://en.wikipedia.org/wiki/Circular_shift
     def left_rotate(word, count)
         ((word << count) | (word >> 32 - count))
     end
 
+    # take the inital 16 words from the block and generate 64 more of them
     def append_words(words)
         (16...80).each do |i|
             new_word = words[i-3] ^ words[i-8] ^ words[i-14] ^ words[i-16]
@@ -43,13 +51,18 @@ class Sha1
         words
     end
 
+    # this is the core compression function of SHA1
+    # runs over the 80 word block that was generated from the initial message block
+    # and calcutes new seed values for the next block
     def compression(words)
+        # hash words for this round:
         a = @hash_words[:a]
         b = @hash_words[:b]
         c = @hash_words[:c]
         d = @hash_words[:d]
         e = @hash_words[:e]
 
+        # run 80 rounds of compression
         (0...80).each do |i|
             if i <= 19
                 f = (b & c) ^ ((~b) & d)
@@ -73,6 +86,8 @@ class Sha1
         end
 
         # Add this chunk's hash to result so far:
+        # make sure it stays at 32 bit
+        # since ruby likes to convert to BigNum class
         @hash_words[:a] = (@hash_words[:a] + a) & (2**32) - 1
         @hash_words[:b] = (@hash_words[:b] + b) & (2**32) - 1
         @hash_words[:c] = (@hash_words[:c] + c) & (2**32) - 1
@@ -80,16 +95,11 @@ class Sha1
         @hash_words[:e] = (@hash_words[:e] + e) & (2**32) - 1
     end
 
-    # Produce the final hash value (big-endian) as a 160-bit number
+    # produce the final hash value (big-endian) as a 160-bit number
+    # concat the 5 hash words as hex 
+    # after the compression function ran over all message blocks
     def produce_hash
-        # hh = (h0 leftshift 128) or (h1 leftshift 96) or (h2 leftshift 64) or (h3 leftshift 32) or h4
-        var1 = (@hash_words[:a]).to_s(16)
-        var2 = (@hash_words[:b]).to_s(16)
-        var3 = (@hash_words[:c]).to_s(16)
-        var4 = (@hash_words[:d]).to_s(16)
-        var5 = (@hash_words[:e]).to_s(16)
-        # this is the final hash
-        "#{var1}#{var2}#{var3}#{var4}#{var5}"
+        @hash_words.values.map{ |word| word.to_s(16) }.join
     end
 
     def digest
@@ -98,8 +108,9 @@ class Sha1
         split_up(@bitstring, 512) do |chunk|
             # break chunk into sixteen 32-bit big-endian words
             words = split_up(chunk, 32)
+            # convert bitstrings to 32-bit integers for easier math
             words = words.map { |word| word.to_i(2) }
-            # extend the sixteen 32-bit words into eighty 32-bit words:
+            # extend the sixteen words into eighty words:
             words = append_words(words)
             # go into main loop
             compression(words)
@@ -108,6 +119,9 @@ class Sha1
     end
 end
 
-ARGV.each do |message|
-    puts("#{Sha1.new(File.open(message).read()).digest()}  #{message}")
+# read the paths as param from the cmd line
+ARGV.each do |path|
+    message = File.open(path).read()
+    hash = Sha1.new(message).digest
+    puts("#{hash}  #{path}")
 end
